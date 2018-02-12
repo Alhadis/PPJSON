735c1779893ed67ddc7b1f824cad1fd3897f7c57
Alhadis <gardnerjohng@gmail.com>
2018-02-13 03:37:01 +1100

    Tidy source code and make better use of ES6 syntax

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const getOpts      = require("get-options");
 const fs           = require("fs");
 
 
-/** Read CLI args */
-let _ = getOpts(process.argv.slice(2), {
+// Read CLI args
+const {options, argv} = getOpts(process.argv.slice(2), {
 	"-?, -h, --help":                       "",
 	"-m, --mutilate":                       "<bool>",
 	"-u, --underline-urls":                 "<bool>",
@@ -17,16 +17,12 @@ let _ = getOpts(process.argv.slice(2), {
 	"-a, --alphabetise":                    ""
 });
 
-/** Would've used `let {options, argv} = getOpts`, but hey, no native destructuring support yet */
-let options       = _.options;
-let argv          = _.argv;
 
-
-/** Print help and bail if that's what the user wanted */
+// Print help and bail if that's what the user wanted
 if(options.help){
 	const path = require("path");
-	let name   = path.basename(process.argv[1]);
-	let help   = `
+	const name = path.basename(process.argv[1]);
+	const help = `
 	Usage: ${name} [options] <filename>
 
 	Pretty-print JSON data for terminal output.
@@ -42,72 +38,83 @@ if(options.help){
 
 	Run \`man ppjson' for full documentation.
 	`.replace(/\t+/g, "    ");
-	
-	console.log(help);
+
+	process.stdout.write(help + "\n");
 	process.exit(0);
 }
 
-/** Parse our options and normalise their defaults */
-let mutilate      = options.m === undefined ? true  : bool(options.m);
-let underlineURLs = options.u === undefined ? true  : bool(options.u);
-let colourise     = options.c === undefined ? true  : bool(options.c);
-let pagedView     = options.p === undefined ? false : bool(options.p);
-let indentSize    = options.i === undefined ? 4     : options.indent;
-let alphabetise   = options.a === undefined ? false : options.alphabetise;
-let indent        = Array(Math.max(1, indentSize) + 1).join(" ");
+// Parse our options and normalise their defaults
+const mutilate      = undefined === options.m ? true  : bool(options.m);
+const underlineURLs = undefined === options.u ? true  : bool(options.u);
+const colourise     = undefined === options.c ? true  : bool(options.c);
+const pagedView     = undefined === options.p ? false : bool(options.p);
+const indentSize    = undefined === options.i ? 4     : options.indent;
+const alphabetise   = undefined === options.a ? false : options.alphabetise;
+const indent        = Array(Math.max(1, indentSize) + 1).join(" ");
 
 
-/** Configure colour palette */
-let env               = process.env;
-const COLOUR_STRINGS  = "\x1B[38;5;" + (+env.PPJSON_COLOUR_STRINGS || 2) + "m";
-const COLOUR_NUMBERS  = "\x1B[38;5;" + (+env.PPJSON_COLOUR_NUMBERS || 2) + "m";
-const COLOUR_TRUE     = "\x1B[38;5;" + (+env.PPJSON_COLOUR_TRUE    || 6) + "m";
-const COLOUR_FALSE    = "\x1B[38;5;" + (+env.PPJSON_COLOUR_FALSE   || 6) + "m";
-const COLOUR_NULL     = "\x1B[38;5;" + (+env.PPJSON_COLOUR_NULL    || 6) + "m";
-const COLOUR_PUNCT    = "\x1B[38;5;" + (+env.PPJSON_COLOUR_PUNCT   || 8) + "m";
-const COLOUR_ERROR    = "\x1B[38;5;" + (+env.PPJSON_COLOUR_ERROR   || 1) + "m";
+// Configure colour palette
+const {env} = process;
+const SGR = {
+	reset:       "\x1B[0m",
+	bold:        "\x1B[1m",
+	unbold:      "\x1B[22m",
+	underline:   "\x1B[4m",
+	noUnderline: "\x1B[24m",
+	colours: {
+		strings: `\x1B[38;5;${ +env.PPJSON_COLOUR_STRINGS || 2 }m`,
+		numbers: `\x1B[38;5;${ +env.PPJSON_COLOUR_NUMBERS || 2 }m`,
+		true:    `\x1B[38;5;${ +env.PPJSON_COLOUR_TRUE    || 6 }m`,	
+		false:   `\x1B[38;5;${ +env.PPJSON_COLOUR_FALSE   || 6 }m`,
+		null:    `\x1B[38;5;${ +env.PPJSON_COLOUR_NULL    || 6 }m`,
+		punct:   `\x1B[38;5;${ +env.PPJSON_COLOUR_PUNCT   || 8 }m`,
+		error:   `\x1B[38;5;${ +env.PPJSON_COLOUR_ERROR   || 1 }m`,
+	},
+};
 
 
 let input  = "";
 let output = "";
 
-/** An input file was specified on command-line */
+// An input file was specified on command-line
 if(argv.length){
-	
-	/** Cycle through each file and prettify their contents */
-	for(let i = 0, l = argv.length; i < l; ++i){
-		let path = argv[i];
+	let separator = "";
+
+	// Cycle through each file and prettify their contents
+	for(const path of argv){
 		
+		// Make sure there'	s enough whitespace between files
+		output += separator;
+		separator = "\n\n";
+
 		try{ fs.accessSync(path); }
 		catch(error){
-			/** If there was an access error, hit eject */
-			process.stderr.write(COLOUR_ERROR);
-			console.error("\x1B[1mERROR\x1B[22m: " + error.message);
-			process.stderr.write("\x1B[0m");
+			// If there was an access error, hit eject
+			process.stderr.write(SGR.colours.error);
+			process.stderr.write(SGR.bold + "ERROR" + SGR.unbold + error.message);
+			process.stderr.write(SGR.reset);
 			process.exit(2);
 		}
 		
-		/** Otherwise, go for it */
+		// Otherwise, go for it
 		output += prettifyJSON(fs.readFileSync(path, {encoding: "utf8"}));
-		
-		/** Make sure there's enough whitespace between files */
-		if(i < l - 1) output += "\n\n";
 	}
 	
-	/** Send the compiled result to STDOUT */
+	// Send the compiled result to STDOUT
 	print(output);
 }
 
-/** No file specified, just read from STDIN instead */
+// No file specified, just read from STDIN instead
 else{
 	process.stdin.setEncoding("utf8");
 	process.stdin.on("readable", () => {
-		let chunk = process.stdin.read();
-		if(chunk !== null)
-		input += chunk;
+		const chunk = process.stdin.read();
+		if(null !== chunk)
+			input += chunk;
 	});
-	
-	process.stdin.on("end", e => print(prettifyJSON(input)));
+	process.stdin.on("end", () => {
+		print(prettifyJSON(input));
+	});
 }
 
 
@@ -119,9 +126,9 @@ else{
  * @return {Boolean}
  */
 function bool(input){
-	let num = +input;
+	const num = +input;
 	
-	/** NaN: String was supplied, check for keywords equating to "false" */
+	// NaN: String was supplied, check for keywords equating to "false"
 	if(num !== num)
 		return !/^(?:false|off|no?|disabled?|nah)$/i.test(input);
 	return !!num;
@@ -140,32 +147,32 @@ function bool(input){
  * @param {Boolean} strictCase - If TRUE, will order case-sensitively (capitals first)
  * @return {Object}
  */
-function alphabetiseProperties(input, strictCase){
-	let stringTag = Object.prototype.toString.call(input);
-	
-	/** Regular JavaScript object; enumerate properties */
+function alphabetiseProperties(input, strictCase = false){
+	const stringTag = Object.prototype.toString.call(input);
+
+	// Regular JavaScript object; enumerate properties
 	if("[object Object]" === stringTag){
 		let keys = Object.keys(input);
 		
 		keys = strictCase ? keys.sort() : keys.sort((a, b) => {
-			let A = a.toLowerCase();
-			let B = b.toLowerCase();
+			const A = a.toLowerCase();
+			const B = b.toLowerCase();
 			if(A < B) return -1;
 			if(A > B) return 1;
 			return 0;
 		});
 		
-		let result = {};
-		for(let i of keys)
-			result[i] = alphabetiseProperties(input[i]);
+		const result = {};
+		for(const key of keys)
+			result[key] = alphabetiseProperties(input[key]);
 		return result;
 	}
 	
-	/** This is an array; make sure the properties of its values are sorted too */
-	else if("[object Array]" === stringTag)
-		return Array.prototype.map.call(input, e => alphabetiseProperties(e));
+	// This is an array; make sure the properties of its values are sorted too
+	else if(Array.isArray(input))
+		return input.map(e => alphabetiseProperties(e));
 	
-	/** Just return it untouched */
+	// Just return it untouched
 	return input;
 }
 
@@ -180,46 +187,45 @@ function alphabetiseProperties(input, strictCase){
 function prettifyJSON(input){
 	let output = JSON.parse(input);
 	
-	/** Order the properties of objects by alphabetical order, not enumeration order */
+	// Order the properties of objects by alphabetical order, not enumeration order
 	if(alphabetise)
 		output = alphabetiseProperties(output);
 	
 	output = JSON.stringify(output, null, indent);
 	
 	
-	/** Unquote property identifiers in object literals */
+	// Unquote property identifiers in object literals
 	if(mutilate){
-		let pattern = new RegExp(indent + '"([\\w\\$]+)":', "g");
-		output      = output.replace(pattern, indent + "$1:");
+		const pattern = new RegExp(indent + '"([\\w\\$]+)":', "g");
+		output = output.replace(pattern, indent + "$1:");
 	}
 	
 	
-	/** Colourise the output */
+	// Colourise the output
 	if(colourise){
-		const RESET = "\x1B[0m";
-		
+		const bracketsPattern = new RegExp("^((?:" + indent + ")*)([\\[\\{\\}\\]],?)", "gm");
+		const {colours, reset} = SGR;
+		const {strings} = colours;
 		output = output
-			.replace(/("([^\\"]|\\.)*")/g, COLOUR_STRINGS + "$1" + RESET)  /** Strings  */
-			.replace(/(\d+,)$/gm,          COLOUR_NUMBERS + "$1" + RESET)  /** Numerals */
+			.replace(/("([^\\"]|\\.)*")/g, colours.strings + "$1" + SGR.reset)  // Strings
+			.replace(/(\d+,)$/gm,          colours.numbers + "$1" + SGR.reset)  // Numerals
 			
-			/** Constants */
-			.replace(/true(,)?$/gm,        COLOUR_TRUE  + "true"  + RESET + "$1")
-			.replace(/false(,)?$/gm,       COLOUR_FALSE + "false" + RESET + "$1")
-			.replace(/null(,)?$/gm,        COLOUR_NULL  + "null"  + RESET + "$1")
+			// Constants
+			.replace(/true(,)?$/gm,        colours.true  + "true"  + SGR.reset + "$1")
+			.replace(/false(,)?$/gm,       colours.false + "false" + SGR.reset + "$1")
+			.replace(/null(,)?$/gm,        colours.null  + "null"  + SGR.reset + "$1")
 			
-			/** Greyed-out unimportant bits */
-			.replace(new RegExp("^((?:"+indent+")*)([\\[\\{\\}\\]],?)", "gm"), "$1"+COLOUR_PUNCT+"$2"+RESET)
-			.replace(/((?:\[\]|\{\})?,)$/gm, COLOUR_PUNCT+"$1"+RESET)
-			.replace(/(\[|\{)$/gm, COLOUR_PUNCT+"$1"+RESET);
+			// Greyed-out unimportant bits
+			.replace(bracketsPattern, "$1" + colours.punct + "$2" + SGR.reset)
+			.replace(/((?:\[\]|\{\})?,)$/gm, colours.punct + "$1" + SGR.reset)
+			.replace(/(\[|\{)$/gm,           colours.punct + "$1" + SGR.reset);
 	}
 	
 
-	/** Underline URLs */
+	// Underline URLs
 	if(underlineURLs){
-		const UNDERLINE_ON  = "\x1B[4m";
-		const UNDERLINE_OFF = "\x1B[24m";
-		let rURL = /(\s*(https?:)?\/\/([^:]+:[^@]+@)?([\w-]+)(\.[\w-]+)*(:\d+)?(\/\S+)?\s*(?="))/gm;
-		output   = output.replace(rURL, UNDERLINE_ON+"$1"+UNDERLINE_OFF);
+		const rURL = /(\s*(https?:)?\/\/([^:]+:[^@]+@)?([\w-]+)(\.[\w-]+)*(:\d+)?(\/\S+)?\s*(?="))/gm;
+		output = output.replace(rURL, SGR.underline + "$1" + SGR.noUnderline);
 	}
 	
 	return output + "\n";
@@ -237,9 +243,9 @@ function prettifyJSON(input){
  */
 function print(input){
 	
-	/** Determine if the output should be piped through to a pager */
+	// Determine if the output should be piped through to a pager
 	if(pagedView && input.match(/\n/g).length > process.stdout.rows){
-		let less  = ChildProcess.spawn("less", ["-Ri"], {
+		const less  = ChildProcess.spawn("less", ["-Ri"], {
 			stdio: ["pipe", process.stdout, process.stderr]
 		});
 		less.stdin.write(input);
